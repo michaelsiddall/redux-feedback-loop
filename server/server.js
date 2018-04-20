e79fc33b766bd31ac172ee9a761ebad30ecbf92a
Christopher Black <chris@primeacademy.io>
2018-04-20 14:47:40 -0500

    Minor updates.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var port = process.env.PORT || 5000;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
