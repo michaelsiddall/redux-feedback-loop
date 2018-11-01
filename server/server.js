a3beee0d7a2cff0890abb3bb7d32832d28cbee47
Dane Smith <doctorhowser@gmail.com>
2018-11-01 14:37:14 -0500

    update cra2, es6

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
@@ -12,6 +12,6 @@ app.use(express.static('build'));
 
 
 /** ---------- START SERVER ---------- **/
-app.listen(port, function () {
-    console.log('Listening on port: ', port);
+app.listen(PORT, () => {
+    console.log('Listening on port: ', PORT);
 });
